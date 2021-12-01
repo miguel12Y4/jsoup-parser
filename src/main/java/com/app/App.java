9d48e77bcdd774b31eb7b76883af922b914a64ae
miguel <miguel.saavedra1601@alumnos.ubiobio.cl>
2021-12-01 19:46:37 -0300

    busqueda en profundidad (poco eficiente)

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -13,24 +13,63 @@ import org.jsoup.select.Elements;
 public final class App {
     
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://es.wikipedia.org/wiki/Algoritmo_de_Dijkstra").get();
-        String title = doc.title();
-        Elements links = doc.getElementsByTag("a");
+
+        String inicio ="https://es.wikipedia.org/wiki/Algoritmo_de_Dijkstra";
+        String meta ="https://commons.wikimedia.org/wiki/1964_New_York_World%27s_Fair";
+
+        String doc = recorrer(inicio, meta);
         
-        System.out.println("link:\t"+doc.baseUri());
-        System.out.println(title);
+        System.out.println(doc);
+
+    }
+
+    public static String recorrer(String inicio, String meta) throws IOException{
+        return camino(inicio, meta, 0,0,0);
+    }
+
+    private static String camino (String url, String meta, int profundidad, int index, int total) throws IOException{
         
-        System.out.println("");
-        for (Element link : links) {
-            String linkHref = link.attr("abs:href");
-
-            if(linkHref != null && !linkHref.equals("") && linkHref.charAt(0) != '#'){
-                System.out.println(linkHref);
-
-            }
-
-            System.out.println("");
+        //objetivo
+        if(url.equals(meta)){
+            System.out.println("Profundidad: "+profundidad);
+            return url;
         }
 
+        //condicion de borde (profundidad maxima de una ruta potencial)
+        if(profundidad==6){
+            return "";
+        }
+
+        //captura de un error al acceder a la url
+        Document doc =null;
+
+        try{
+            doc = Jsoup.connect(url).get();
+        }catch(Exception e){
+            return "";
+        }
+
+        //obtener todos los enlaces de la pagina
+        Elements links = doc.getElementsByTag("a");
+
+        System.out.println("la profundidad es:"+profundidad+"."+index+"/"+total);
+
+        int i=0;
+        System.out.println(url);
+        for (Element link : links) {
+            String linkHref = link.attr("abs:href");
+            String linksolo = link.attr("href");
+
+            if(linksolo != null && !linksolo.equals("") && linksolo.charAt(0) != '#'){
+                
+                String ruta = camino(linkHref, meta, profundidad+1, i, links.size());
+               
+                if(ruta!="" && !ruta.contains(url)){
+                    return url+"   "+ruta;
+                }
+            }
+            i++;
+        }
+        return "";
     }
 }
