3ddceec3369abf86b53b9a577a8d14a885e030c6
miguel <miguel.saavedra1601@alumnos.ubiobio.cl>
2021-12-01 18:30:26 -0300

    cuarto commit

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -16,19 +16,19 @@ public final class App {
         Document doc = Jsoup.connect("https://es.wikipedia.org/wiki/Algoritmo_de_Dijkstra").get();
         String title = doc.title();
         Elements links = doc.getElementsByTag("a");
+        
         System.out.println("link:\t"+doc.baseUri());
         System.out.println(title);
         
         System.out.println("");
         for (Element link : links) {
             String linkHref = link.attr("abs:href");
-            String linkText = link.text();
-            if(linkHref != null && !linkHref.equals("") && linkHref.charAt(0) == '#'){
-                //no es imprime nada ya que no es un link, es solo para ir a un punto en especifico
-                //de la pagina
-            }else{
-                System.out.println(linkText+"\n"+linkHref);
+
+            if(linkHref != null && !linkHref.equals("") && linkHref.charAt(0) != '#'){
+                System.out.println(linkHref);
+
             }
+
             System.out.println("");
         }
 
